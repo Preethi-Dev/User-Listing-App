4c66a445126de261df07b4b5602e53de3c37757f
Preethi <selvampreethi360@gmail.com>
2024-11-01 21:55:50 +0530

    display users in table

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import DisplayUsers from './users/components/DisplayUsers';
 
 const darkTheme = createTheme({
   palette: {
@@ -13,7 +14,7 @@ function App() {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
     <div className="App">
-      
+      <DisplayUsers />
     </div>
     </ThemeProvider>
   );
--- src/users/components/DisplayUsers.tsx
+++ src/users/components/DisplayUsers.tsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { UserList } from '../users.modal';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+
 
 const DisplayUsers = (): JSX.Element => {
+  const [userList, setUserList] = useState<UserList>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try{
+      const response = await fetch("/users.json");
+      if(response.ok){
+        const data = await response.json();    
+        setUserList(data);
+      }
+
+      }catch(err){
+        console.error("Error fetching data:", err);
+      }   
+    }
+
+    fetchUsers()
+  }, [])
+
   return (
-    <div>DisplayUsers</div>
+   <TableContainer>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Username</TableCell>
+          <TableCell>FirstName</TableCell>
+          <TableCell>LastName</TableCell>
+          <TableCell>Email</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {
+          userList.map((user) => 
+          <TableRow key={user.id}>
+            <TableCell>{user.username}</TableCell>
+            <TableCell>{user.firstName}</TableCell>
+            <TableCell>{user.lastName}</TableCell>
+            <TableCell>{user.email}</TableCell>
+          </TableRow>)
+        }
+      </TableBody>
+    </Table>
+   </TableContainer>
   )
 }
 
